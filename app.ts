50fbc9f676e409f9b8dad9f9d7e125147707580f
Dmytro Bohdanov <49651527+DmytroBohdanow@users.noreply.github.com>
2023-05-07 10:04:21 +0200

    initial commit
    server set up

---
+++ app.ts
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'client/index.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + 'client/login.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
